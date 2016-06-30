9a0f304b4cff7a990c0bab3a1f9ab501574ab524
Kent <kentqlee@gmail.com>
2016-06-30 16:47:59 -0700

    Built out event search functionality

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const request = require('request');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const baseURL = 'https://www.eventbriteapi.com/v3/';
+const URLs = {
+  eventSearch: baseURL + 'events/search/?token=' + process.env.EVENTBRITE_PERSONAL_OAUTH_TOKEN,
+};
+
+const Eventbrite = {
+
+  SearchEvents: (query, callback) => {
+    request({
+      url: 'https://www.eventbriteapi.com/v3/events/search',
+      headers: {
+        Authorization: 'Bearer ' + process.env.EVENTBRITE_PERSONAL_OAUTH_TOKEN,
+      },
+      qs: {
+        q: query,
+      },
+      method: 'GET',
+    }, (err, res, data) => {
+      if (err) {
+        callback(err);
+      }
+      callback(null, data);
+    });
+  },
+
+};
+
+module.exports = Eventbrite;
